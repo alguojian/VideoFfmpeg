3f2ab7f04afa809665c1d38463a367bcea4cd5b5
alguojian <jzhszy@foxmail.com>
2020-06-02 11:49:40 +0800

    ..

--- app/src/main/java/com/alguojian/simple/MainActivity.kt
+++ app/src/main/java/com/alguojian/simple/MainActivity.kt
@@ -14,6 +14,7 @@ import com.alguojian.videoffmpeg.trim.VfVideoListener
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
